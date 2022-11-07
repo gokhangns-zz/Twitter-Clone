7f0f87a39df8f9613b46ffd3a3e606a8bd724985
Gökhan GÜNEŞ <gokhan_gns_@hotmail.com>
2022-11-07 23:10:31 +0300

    Twitter Icons Added

--- twitter-clone/src/Layout/Content.js
+++ twitter-clone/src/Layout/Content.js
@@ -4,9 +4,10 @@ const Content = () => {
   return (
     <main className='bg-blue-500 flex-1'>
       <h1>Content</h1>
+      
 
     </main>
   )
 }
 
-export default Content
+export default Content;
--- twitter-clone/src/Layout/SideBar.js
+++ twitter-clone/src/Layout/SideBar.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { HomeIcon, ExploreIcon, NotificationsIcon, MessagesIcon, BookmarksIcon, ListsIcon, ProfileIcon, MoreIcon } from '../icons/Icon'
 
 const SideBar = () => {
   return (
     <div className='w-72 bg-blue-200'>
       <h1>Sidebar</h1>
+      <HomeIcon />
+      <ExploreIcon/>
+      <NotificationsIcon/>
+      <MessagesIcon/>
+      <BookmarksIcon/>
+      <ListsIcon/>
+      <ProfileIcon/>
+      <MoreIcon/>
+
     </div>
   )
 }
